8faf85e372de8c723521fba45bfddc73728f4f87
Krzychor <37249796+Krzychor@users.noreply.github.com>
2019-05-13 23:34:45 +0200

    animation controller
    basic animator controller

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator animator;
+    float ToLookAround;
+
+
+    void Start()
+    {
+        ToLookAround = Random.Range(5, 20);
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Horizontal") > 0)
+            animator.SetBool("IsMoving", true);
+        else
+        {
+            animator.SetBool("IsMoving", false);
+            ToLookAround -= Time.deltaTime;
+            if (ToLookAround < 0)
+            {
+                ToLookAround = Random.Range(5, 20);
+                animator.SetTrigger("Look Around");
+            }
+        }
+        if(Input.GetAxis("Sprint") > 0)
+            animator.SetBool("IsSprinting", true);
+        else
+            animator.SetBool("IsSprinting", false);
+
+    }
+}
