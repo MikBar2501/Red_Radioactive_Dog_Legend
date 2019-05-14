129c4b9f13c40bddf693c6503589a89cd5a31f1a
MikBar2501 <45071328+MikBar2501@users.noreply.github.com>
2019-05-14 13:45:03 +0200

    Zone and damage
    Scripts for zone and zone prefab. Zone settings for player

---
+++ Assets/Scripts/PlayerRadiationSettings.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRadiationSettings : MonoBehaviour
+{
+    
+    public int shieldCount;
+    public float shieldValue;
+    public float maxShieldValue;
+    public int nonShieldInZoneCounter;
+    public int maxNonShieldInZoneCounter;
+    public bool inZone;
+    public bool isDead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shieldValue = maxShieldValue;
+        isDead = false;
+        //shieldCount = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetInZone(bool imInZone) {
+        inZone = imInZone;
+    }
+
+    public void SetDamage(float distance, float damage) {
+        //Debug.Log("Damage: " + damage + ", distance: " + distance);
+        if(shieldValue <= 0 && shieldCount <= 0) {
+            shieldValue = 0;
+            shieldCount = 0;
+            if(!isDead){
+                nonShieldInZoneCounter++;
+                if(nonShieldInZoneCounter >= maxNonShieldInZoneCounter) {
+                    Death();
+                    isDead = true;
+                }
+            }
+        } else {
+            if(shieldCount > 0) {
+                shieldValue -= damage;
+                if(shieldValue <= 0) {
+                    shieldCount--;
+                    shieldValue = maxShieldValue + shieldCount;
+                }
+            } else {
+                shieldValue -= damage;
+            }
+        }
+        Debug.Log("Shield value: " + shieldValue + ", Shield count: " + shieldCount);
+    }
+
+    public void AddResistance(int value) {
+         
+        shieldValue += value;
+        if(shieldValue > maxShieldValue) {
+            shieldCount++;
+            shieldValue = shieldValue - maxShieldValue;
+        }
+    }
+
+    public void Death() {
+        Debug.Log("You died, bitch");
+    }
+
+
+}
---
+++ Assets/Scripts/ZoneScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneScript : MonoBehaviour
+{
+    public bool inZone;
+    public int radiationStrengthMax;
+    float distance;
+    Collider player;
+    float radius;
+    
+    void Start()
+    {
+        radius = GetComponent<SphereCollider>().radius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Player")) {
+            player = other;
+            inZone = true;
+            other.GetComponent<PlayerRadiationSettings>().SetInZone(inZone);
+            InvokeRepeating("SetDamage",1f,1f);
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.CompareTag("Player")) {
+            player = null;
+            inZone = false; 
+            other.GetComponent<PlayerRadiationSettings>().SetInZone(inZone); 
+            CancelInvoke("SetDamage");
+        } 
+    }
+
+    void OnTriggerStay(Collider other) {
+        if(other.CompareTag("Player")) {
+            distance = Vector3.Distance(transform.position,other.GetComponent<Transform>().transform.position);
+        }
+    }
+
+    void Print() {
+        print(Time.time);
+        Debug.Log("COś");
+    }
+
+    void SetDamage() {
+        float x = distance / radius;
+        float damage = (1-x) * radiationStrengthMax;
+        if(damage < 0) {
+            damage *= -1;
+        }
+        player.GetComponent<PlayerRadiationSettings>().SetDamage(distance,damage);
+    }
+}
